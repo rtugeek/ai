47b8ce11f6e81b9b478a6473d33b1588ff9af1af
Neo Fu <rtugeek@gmail.com>
2023-10-30 09:32:42 +0800

    生成包名组件配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,35 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path'
 import ViteWidget from '@widget-js/vite-plugin-widget'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-    base: '/ai',
-    build: {
-        target: "es6",
-        rollupOptions: {
-            output: {
-                entryFileNames: `assets/[name].js`,
-                chunkFileNames: `assets/[name].js`,
-                assetFileNames: `assets/[name].[ext]`
-            }
-        }
-    },
-    plugins: [vue(), ViteWidget(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        })],
-    resolve: {
-        alias: [{find: '@', replacement: path.resolve(__dirname, 'src')}]
+  base: '/ai',
+  build: {
+    target: 'es6',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
     }
+  },
+  plugins: [
+    vue(),
+    ViteWidget({ generateFullNamePackage: true }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+  }
 })
