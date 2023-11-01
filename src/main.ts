b681924f075ae9249f41a9d6e5e4a3686ec8484c
Neo Fu <rtugeek@gmail.com>
2023-11-01 20:43:12 +0800

    show loading progress

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,9 @@ import '@widget-js/vue3/dist/style.css'
 import '@widget-js/vue3/dist/mingcute_icon/MingCute.css'
 import 'element-plus/dist/index.css'
 import '@icon-park/vue-next/styles/index.css';
+import 'nprogress/nprogress.css'
+import NProgress from "nprogress";
+NProgress.configure({ trickle: false });
+
 createApp(App).use(WidgetJsPlugin).use(router).mount('#app')
 
--- src/widgets/ai/ChatGptWidgetView.vue
+++ src/widgets/ai/ChatGptWidgetView.vue
@@ -1,29 +1,31 @@
 <template>
   <!--    <chatgpt-search-widget></chatgpt-search-widget>-->
-  <div class="wrapper">
-    <webview src="https://chat.openai.com" partition="persist:cn.widgetjs.widgets.ai.assistant"/>
+  <div class="wrapper" :style="{transform: `translateX(${animationX}vw)`}">
+    <webview ref="webView" src="localhost" partition="persist:cn.widgetjs.widgets.ai.assistant" />
     <div class="background"></div>
     <tip></tip>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {BrowserWindowApi, ShortcutApi} from "@widget-js/core";
-import {useShortcutListener, useWidget} from "@widget-js/vue3";
-import {computed, nextTick, onMounted, ref, watch} from "vue";
-import {TransitionPresets, useTransition} from "@vueuse/core";
-import {ChatGptConfigData} from "@/widgets/model/ChatGptConfigData";
-import Tip from "@/components/Tip.vue";
-
+import { BrowserWindowApi, ShortcutApi } from '@widget-js/core'
+import { useShortcutListener, useWidget } from '@widget-js/vue3'
+import { computed, nextTick, onMounted, ref, watch } from 'vue'
+import { TransitionPresets, useTransition } from '@vueuse/core'
+import { ChatGptConfigData } from '@/widgets/model/ChatGptConfigData'
+import Tip from '@/components/Tip.vue'
+import NProgress from 'nprogress'
+import { delay } from '@/utils/TimeUtils'
+NProgress.start()
 const shortcut = ref<string>('')
-const proxyRule = ref<string>('')
-const {widgetData, widgetParams, sizeStyle} = useWidget(ChatGptConfigData, {
+const webView = ref<Electron.WebviewTag>()
+const { widgetData, widgetParams } = useWidget(ChatGptConfigData, {
   loadDataByWidgetName: true,
   onDataLoaded: async (data) => {
     if (data) {
       if (data.shortcut != shortcut.value) {
-        ShortcutApi.unregister(shortcut.value);
-        shortcut.value = data.shortcut;
+        ShortcutApi.unregister(shortcut.value)
+        shortcut.value = data.shortcut
         const result = await ShortcutApi.register(shortcut.value)
       }
 
@@ -33,38 +35,59 @@ const {widgetData, widgetParams, sizeStyle} = useWidget(ChatGptConfigData, {
         await BrowserWindowApi.setProxy({
           proxyRules: data.getProxyRule()
         })
+        if (webView.value) {
+          webView.value.reload()
+        }
       }
     }
   }
-});
-const x = ref(screen.width)
+})
+const x = ref(0)
 const windowWidth = screen.width / 3
 
 const animationX = useTransition(x, {
   duration: 500,
-  transition: TransitionPresets.easeInOutCubic,
+  transition: TransitionPresets.easeInOutCubic
 })
 
 onMounted(async () => {
-  await nextTick();
-  show();
+  await nextTick()
+  show()
+  await delay(500)
+  if (webView.value) {
+    webView.value!.addEventListener('did-start-navigation', () => {
+      NProgress.start()
+      console.log('did-start-loading')
+    })
+    webView.value.addEventListener('did-frame-finish-load', () => {
+      console.log('did-frame-finish-load')
+      NProgress.done()
+    })
+    webView.value.loadURL('https://chat.openai.com')
+  }
 })
 
 watch(animationX, () => {
-  BrowserWindowApi.setBounds({x: animationX.value})
+  // BrowserWindowApi.setBounds({x: animationX.value})
+  if (animationX.value == 100) {
+    BrowserWindowApi.hide();
+  }
 })
 
 const show = () => {
-  x.value = screen.width - windowWidth
+  BrowserWindowApi.show();
+  x.value = 0;
+  console.log('show')
 }
 
 const hide = () => {
-  x.value = screen.width
+  x.value = 100
+  console.log('hide')
 }
 const setupWindow = async () => {
   await BrowserWindowApi.setAlwaysOnTop(true)
   await BrowserWindowApi.setBounds({
-    x: x.value,
+    x: screen.availWidth - windowWidth,
     y: 0,
     width: windowWidth,
     height: screen.availHeight
@@ -74,16 +97,16 @@ const setupWindow = async () => {
 setupWindow()
 
 const isShowing = computed(() => {
-  return x.value == screen.width - windowWidth
+  return x.value == 0
 })
 
 useShortcutListener((shortcut: string) => {
   if (isShowing.value) {
     hide()
-    BrowserWindowApi.blur();
+    BrowserWindowApi.blur()
   } else {
-    show();
-    BrowserWindowApi.focus();
+    show()
+    BrowserWindowApi.focus()
   }
 })
 </script>
@@ -96,6 +119,7 @@ $padding: 16px;
   height: 100vh;
   position: relative;
   padding: 16px;
+  transition: all 0.5ms ease-out;
   background: rgba(0, 0, 0, 0.3);
 
   .background {
@@ -106,7 +130,6 @@ $padding: 16px;
     background: white;
   }
 
-
   webview {
     width: calc(100% - 16px * 2);
     height: calc(100% - 16px * 2);
@@ -114,5 +137,4 @@ $padding: 16px;
     position: absolute;
   }
 }
-
 </style>
