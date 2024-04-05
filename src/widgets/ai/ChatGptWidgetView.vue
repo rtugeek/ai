bff31b045280015bfa3144cb160d2e418684b604
Neo Fu <rtugeek@gmail.com>
2024-04-05 11:48:10 +0800

    :bookmark: Updated WebView user agent string

--- src/widgets/ai/ChatGptWidgetView.vue
+++ src/widgets/ai/ChatGptWidgetView.vue
@@ -118,7 +118,7 @@ useShortcutListener((_: string) => {
 <template>
   <!--    <chatgpt-search-widget></chatgpt-search-widget> -->
   <div class="wrapper" :style="{ transform: `translateX(${animationX}vw)` }">
-    <webview ref="webView" src="localhost" partition="persist:cn.widgetjs.widgets.ai.assistant" />
+    <webview ref="webView" useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36" src="localhost" partition="persist:cn.widgetjs.widgets.ai.assistant" />
     <div class="background" />
     <Tip />
   </div>
